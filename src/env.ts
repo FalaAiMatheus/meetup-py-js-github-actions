9ddc9b2f6c554a5a7a208f851b52bd19a8b3f387
FalaAiMatheus <matheussfrancads2345@gmail.com>
2025-03-28 20:48:29 -0300

    feat: configure drizzle

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
 
 const schema = z.object({
-    API_PORT: z.coerce.number().min(1)
+    API_PORT: z.coerce.number().min(1),
+    DATABASE_URL: z.string().url().min(1)
 }).parse(process.env)
 
 export { schema as env };
